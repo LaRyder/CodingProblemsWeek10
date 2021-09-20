f37aa1aa645ae7e094f39066e7f503f65055f2cd
Lauren Ryder <lauren.ryder@microsoft.com>
2021-09-20 11:55:58 -0400

    adding example 3

---
+++ CodingProblems/CodingProblems/DoubleExample1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingProblems
+{
+    public class DoubleExample1
+    {
+        public bool Check(int[] nums)
+        {
+            {
+                for (var i = 0; i < nums.Length; i++)
+                {
+                    for (var k = 0; k < nums.Length; k++)
+                    {
+                        if (nums[i] * 2 == nums[k]) return true; 
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ CodingProblems/CodingProblems_tests/DoubleExample1_test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingProblems;
+
+
+namespace CodingProblems_tests
+{
+    [TestClass]
+    public class DoubleExample1_tests
+    {
+        [TestMethod]
+        public void Check_isDouble_ReturnsTrue()
+        {
+            var sut = new CodingProblems.DoubleExample1();
+            // var input = new int[] { 10, 2, 5, 3 };
+            // var input = new int[] { 7, 1, 14, 11 };
+            var input = new int[] { 3, 1, 7, 11 };
+            var expect = false;
+
+            var actual = sut.Check(input);
+
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
